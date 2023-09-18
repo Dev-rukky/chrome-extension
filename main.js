e639ea98a527f7e6d8a02873a59eee6da7696077
Agbadudu Oghenerukevwe <90446020+Dev-rukky@users.noreply.github.com>
2023-09-18 14:49:37 +0100

    hmmm

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-// let myLeads = []
-// const inputEl = document.getElementById("input-el")
-// const inputBtn = document.getElementById("input-btn")
-// const ulEl = document.getElementById("ul-el")
-
-// inputBtn.addEventListener("click", function() {
-//     myLeads.push(inputEl.value)
-//     inputEl.value = ""
-//     renderLeads()
-// })
-
-// function renderLeads() {
-//     let listItems = ""
-//     for (let i = 0; i < myLeads.length; i++) {
-//         listItems += `
-//             <li>
-//                 <a target='_blank' href='${myLeads[i]}'>
-//                     ${myLeads[i]}
-//                 </a>
-//             </li>
-//         `
-//     }
-//     ulEl.innerHTML = listItems  
-// }
-
 let leadsInput = [];
 const inputElement = document.getElementById("input-el");
 const saveInputBtn = document.getElementById("input-btn");
